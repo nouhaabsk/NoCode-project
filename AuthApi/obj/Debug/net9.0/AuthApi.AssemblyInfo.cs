30b3f0cebecfa015ca73b18a79efbb58be2fafcb
Nouha <nouhaboussoukaya@gmail.com>
2025-07-14 09:10:05 +0100

    Initial commit: frontend and backend

--- AuthApi/obj/Debug/net9.0/AuthApi.AssemblyInfo.cs
+++ AuthApi/obj/Debug/net9.0/AuthApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05c6d693e47f80f116fd032ed458f28db9563c9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
